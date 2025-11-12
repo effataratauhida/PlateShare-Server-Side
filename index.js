ccff7c77c2a5ff93981e7706b64616a61138ea83
Effat Ara Tauhida <effataratauhida2020@gmail.com>
2025-11-12 18:35:40 +0600

    added feature to store new food data in MongoDB

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
+
+const uri = process.env.MONGO_URI;
+
 //middleware
 app.use(cors())
 app.use(express.json());
-
-// const uri = "mongodb+srv://myProjectUser:<db_password>@cluster0.vdlk0az.mongodb.net/?appName=Cluster0";
-const uri = "mongodb+srv://myProjectUser:95Jh4ln9Or1QxsVc@cluster0.vdlk0az.mongodb.net/?appName=Cluster0";
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,26 +21,29 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const db = client.db('plateshare-db');
-    const foodCollection = db.collection('foodData')
+    const foodCollection = db.collection('foodData');
 
-    app.get('/foodData', async (req,res) => {
-      const result = await foodCollection.find().toArray()
+  //get all food
+  app.get('/foodData', async (req,res) => {
+    const result = await foodCollection.find().toArray()
       res.send(result)
     })
     
+   // POST new food (from AddFood.jsx)
+    app.post('/foodData', async (req, res) => {
+      const newFood = req.body;
+      const result = await foodCollection.insertOne(newFood);
+      res.send(result);
+    });
 
-
-
-
-
-
-    // Send a ping to confirm a successful connection
+// Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     //console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -59,5 +63,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, () => {
-  //console.log(`Server is listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
